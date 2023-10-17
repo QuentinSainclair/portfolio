d8f4a6fa313bcf80fa33f4a74e45a91c8eb9558f
Quentin <qsainclair@gmail.com>
2023-10-17 23:17:00 +0200

    home revue et skills 50%

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent {
-
+  imgFrontDesktop: string;
+  imgBackDesktop: string;
+  imgMultitache: string;
+  constructor() {
+    this.imgFrontDesktop = 'assets/Images/front_desktop.png';
+    this.imgBackDesktop = 'assets/Images/back_desktop.png';
+    this.imgMultitache = 'assets/Images/multitache.svg'
+  }
 }
