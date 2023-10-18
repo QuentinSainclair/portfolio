2d0ad116c13dd3edc07d999645555771f09c45bc
Quentin <qsainclair@gmail.com>
2023-10-18 15:19:37 +0200

    animation skills à finir

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-skills',
@@ -9,9 +10,27 @@ export class SkillsComponent {
   imgFrontDesktop: string;
   imgBackDesktop: string;
   imgMultitache: string;
+  imgTitle: string;
+  imgToolsDesktop: string;
   constructor() {
     this.imgFrontDesktop = 'assets/Images/front_desktop.png';
     this.imgBackDesktop = 'assets/Images/back_desktop.png';
-    this.imgMultitache = 'assets/Images/multitache.svg'
+    this.imgMultitache = 'assets/Images/multitache.svg';
+    this.imgTitle = 'assets/Images/Skills.png';
+    this.imgToolsDesktop = 'assets/Images/tools_desktop.png';
+  }
+  ngAfterViewInit() {
+    let image = document.getElementById('imgMultitache');
+    let texte = document.getElementById('textePresentationSkills');
+    let imgSkillsFrontDesktop = document.getElementById('imgSkillsFrontDesktop');
+
+    const TL1 = gsap.timeline({paused: true});
+
+    TL1
+      .from(image, { x: -100, opacity: 0, duration: 0.5,})
+      .from(texte, { x: 100, opacity: 0, duration: 0.5,})
+      .from(imgSkillsFrontDesktop, { y: 100, opacity: 0, duration: 0.5,})
+
+    TL1.play();
   }
 }
